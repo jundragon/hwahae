3c73dfc7248c69bc5ee014f27605a667064ec582
jundragon <jjundragon88@gmail.com>
2020-01-28 01:49:18 +0900

    heroku mysql 오류

--- hwahae_api/settings/production.py
+++ hwahae_api/settings/production.py
@@ -1,12 +1,15 @@
 import os
+from django.db.backends.mysql.base import DatabaseWrapper
 from .base import *
 
+DatabaseWrapper.data_types["DateTimeField"] = "datetime"  # fix for MySQL 5.5
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': os.environ['MYSQL_ROOT_HOST'],
-        'NAME': os.environ['MYSQL_DATABASE'],
-        'USER': os.environ['MYSQL_USER'],
-        'PASSWORD': os.environ['MYSQL_ROOT_PASSWORD'],
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "HOST": os.environ["MYSQL_ROOT_HOST"],
+        "NAME": os.environ["MYSQL_DATABASE"],
+        "USER": os.environ["MYSQL_USER"],
+        "PASSWORD": os.environ["MYSQL_ROOT_PASSWORD"],
     }
 }
